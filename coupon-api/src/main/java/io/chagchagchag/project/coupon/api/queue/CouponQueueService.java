d2512bd217e1358b67d7305486395330659c73ad
chagchagchag <chagchagchag.dev@gmail.com>
2024-02-05 16:38:04 +0900

    redis config

--- coupon-api/src/main/java/io/chagchagchag/project/coupon/api/queue/CouponQueueService.java
+++ coupon-api/src/main/java/io/chagchagchag/project/coupon/api/queue/CouponQueueService.java
@@ -1,12 +1,15 @@
 package io.chagchagchag.project.coupon.api.queue;
 
 import io.chagchagchag.project.coupon.api.queue.valueobject.CouponQueueIssueDto;
+import io.chagchagchag.project.coupon.api.redis.RedisRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class CouponQueueService {
+
+    private final RedisRepository redisRepository;
 
     public void enqueue(CouponQueueIssueDto queueDto){
         // redis call 등등
---
+++ coupon-api/src/main/java/io/chagchagchag/project/coupon/api/redis/RedisRepository.java
@@ -0,0 +1,9 @@
+package io.chagchagchag.project.coupon.api.redis;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@RequiredArgsConstructor
+@Repository
+public class RedisRepository {
+}
