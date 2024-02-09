1810e076597fcdb5643bab855525535891b672ee
chagchagchag <chagchagchag.dev@gmail.com>
2024-02-09 15:57:17 +0900

    CouponEntityFactory 의 isIssueDateAvailable, issue 테스트

--- coupon-core/src/test/java/io/chagchagchag/project/coupon/core/dataaccess/entity/factory/CouponEntityFactoryTest.java
+++ coupon-core/src/test/java/io/chagchagchag/project/coupon/core/dataaccess/entity/factory/CouponEntityFactoryTest.java
@@ -1,6 +1,8 @@
 package io.chagchagchag.project.coupon.core.dataaccess.entity.factory;
 
 import io.chagchagchag.project.coupon.core.dataaccess.entity.CouponEntity;
+import io.chagchagchag.project.coupon.core.exception.CouponIssueException;
+import io.chagchagchag.project.coupon.core.exception.ErrorCode;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -11,6 +13,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Transactional
 @ActiveProfiles("test")
@@ -65,41 +68,88 @@ public class CouponEntityFactoryTest {
     @Test
     @DisplayName("isIssueDateAvailable() : 발급 기간이 정상일 경우 true 를 리턴")
     void isIssueDateAvailable__RETURN_TRUE_WHEN_ISSUE_DATE_IS_NORMAL_STATE(){
+        // given
+        CouponEntity coupon = couponEntityFactory
+                .newDefaultCouponWithExpiration(
+                        "농심 10% 할인 이벤트",
+                        LocalDateTime.now().minusDays(1), LocalDateTime.now().plusDays(2)
+                );
 
+        // when
+        final boolean isIssueDateAvailable = coupon.isIssueDateAvailable();
+
+        // then
+        Assertions.assertTrue(isIssueDateAvailable);
     }
 
     @Test
     @DisplayName("isIssueDateAvailable() : 발급 기간이 종료된 경우 false 를 리턴")
     void isIssueDateAvailable__RETURN_FALSE_WHEN_ISSUE_DATE_IS_ABNORMAL_STATE(){
+        // given
+        CouponEntity coupon = couponEntityFactory
+                .newDefaultCouponWithExpiration(
+                        "농심 10% 할인 이벤트",
+                        LocalDateTime.now().minusDays(2),
+                        LocalDateTime.now().minusDays(1)
+                );
 
+        // when
+        final boolean isIssueDateAvailable = coupon.isIssueDateAvailable();
+
+        // then
+        Assertions.assertFalse(isIssueDateAvailable);
     }
 
     @Test
     @DisplayName("issue() : 발급 수량, 발급 기간이 유효하면 발급 성공")
     void issue__ISSUE_SUCCESS_WHEN_QUANTITY_AND_DURATION_IS_NORMAL(){
+        // given
+        CouponEntity coupon = couponEntityFactory
+                .newDefaultCouponWithExpiration(
+                    "농심 10% 할인 이벤트",
+                    BigDecimal.TWO, BigDecimal.ONE,
+                    LocalDateTime.now().minusDays(1), LocalDateTime.now().plusDays(1)
+                );
 
+        // when
+        coupon.issue();
+        // then
+        Assertions.assertEquals(coupon.getIssuedQuantity(), BigDecimal.ONE);
     }
 
     @Test
     @DisplayName("issue() : 발급 기간이 유효해도 발급 수량을 초과하면 예외 throw")
     void issue__ISSUE_EXCEPTION_THROW_WHEN_DURATION_NORMAL_BUT_QUANTITY_UNAVAILABLE(){
+        // given
+        CouponEntity coupon = couponEntityFactory
+                .newDefaultCouponWithExpiration(
+                        "농심 10% 할인 이벤트",
+                        BigDecimal.ONE, BigDecimal.TWO,
+                        LocalDateTime.now().minusDays(1), LocalDateTime.now().plusDays(1)
+                );
 
+        // when, then
+        CouponIssueException exception = Assertions.assertThrows(CouponIssueException.class, coupon::issue);
+        Assertions.assertEquals(exception.getErrorCode(), ErrorCode.EXCEEDED_COUPON_ISSUE_QUANTITY);
     }
 
     @Test
     @DisplayName("issue() : 발급 기간이 비정상이면 예외를 throw")
     void issue__ISSUE_EXCEPTION_THROW_WHEN_DURATION_ABNORMAL(){
+        // given
+        CouponEntity coupon = couponEntityFactory
+                .newDefaultCouponWithExpiration(
+                        "농심 10% 할인 이벤트",
+                        BigDecimal.TWO, BigDecimal.ONE,
+                        LocalDateTime.now().plusDays(1), LocalDateTime.now().plusDays(2)
+                );
 
+        // when, then
+        CouponIssueException exception = Assertions.assertThrows(CouponIssueException.class, coupon::issue);
+        Assertions.assertEquals(exception.getErrorCode(), ErrorCode.UNAVAILABLE_COUPON_ISSUE_DATE);
     }
 
-    ///// 3) 발급 기한 만료 검증 메서드 테스트
-    @Test
-    @DisplayName("isExpiredCoupon() : 발급 기간이 종료된 경우 isExpiredCoupon() 은 true 를 리턴한다.")
-    void isExpiredCoupon__WHEN_ISSUE_EXPIRED_THEN_RETURN_TRUE(){
-
-    }
-
-    ///// 4) isCouponQuantityFull
+    ///// 3) isCouponQuantityFull
 
 
 }
