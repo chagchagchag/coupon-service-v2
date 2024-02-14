93228ef41f8f5dd7a68eec778b4c8b3a1f2c626c
chagchagchag <chagchagchag.dev@gmail.com>
2024-02-14 12:53:16 +0900

    typo

--- coupon-issuer/src/main/java/io/chagchagchag/project/coupon/issuer/CouponIssuerApplication.java
+++ coupon-issuer/src/main/java/io/chagchagchag/project/coupon/issuer/CouponIssuerApplication.java
@@ -5,20 +5,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories(
         basePackages = {
-                "io.chagchagchag.project.coupon.core.dataaccess",
                 "io.chagchagchag.project.coupon.dataaccess"
         }
 )
 @EntityScan(
         basePackages = {
-                "io.chagchagchag.project.coupon.core.dataaccess",
                 "io.chagchagchag.project.coupon.dataaccess"
         }
 )
+@EnableJpaAuditing
 @EnableAspectJAutoProxy(exposeProxy = true)
 @EnableCaching
 @SpringBootApplication(scanBasePackages = "io.chagchagchag.project.coupon")
