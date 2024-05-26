bf5c3e6545cd109e128f11e0ef545ca67893d20e
chagchagchag <chagchagchag.dev@gmail.com>
2024-05-26 12:01:16 +0900

    jib build 추가

--- coupon-core/build.gradle.kts
+++ coupon-core/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.2.2"
 	id("io.spring.dependency-management") version "1.1.4"
+	id("com.google.cloud.tools.jib") version "3.4.0"
 }
 
 group = "io.chagchagchag.project"
@@ -52,4 +53,27 @@ tasks.getByName("bootJar"){
 
 tasks.getByName("jar"){
 	enabled = true
+}
+
+jib {
+	from {
+		image = "amazoncorretto:17"
+	}
+	to {
+		// image 도 변수로 지정해서 동적으로 지정하고 싶다면 아래와 같이...
+//		image = "${project.name}-${project.version.toString().toLowerCase()}"
+		// 이번 예제는 아래처럼 정해진 이름은 정해진 이름으로 확실하게 별도 지정합니다.
+		image = "chagchagchag/coupon-core"
+		tags = setOf("0.0.1", "0.0.1.coupon-core.01", "latest")
+	}
+	container {
+		creationTime = "USE_CURRENT_TIMESTAMP"
+
+		// JVM 옵션들
+		// 서버 포트로는 8080을 사용했으며, profile로는 local을 선택했다.
+		jvmFlags = listOf("-Dspring.profiles.active=local", "-XX:+UseContainerSupport", "-Dserver.port=8080", "-Dfile.encoding=UTF-8")
+
+		// 컨테이너 입장에서 자기 자신을 외부로 노출할 포트입니다.
+		ports = listOf("8080")
+	}
 }
--- coupon-issuer/build.gradle.kts
+++ coupon-issuer/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.2.2"
 	id("io.spring.dependency-management") version "1.1.4"
+	id("com.google.cloud.tools.jib") version "3.4.0"
 }
 
 group = "io.chagchagchag.project"
@@ -46,4 +47,26 @@ dependencies {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+jib {
+	from {
+		image = "amazoncorretto:17"
+	}
+	to {
+		// image 도 변수로 지정해서 동적으로 지정하고 싶다면 아래와 같이...
+//		image = "${project.name}-${project.version.toString().toLowerCase()}"
+		// 이번 예제는 아래처럼 정해진 이름은 정해진 이름으로 확실하게 별도 지정합니다.
+		image = "chagchagchag/coupon-issuer"
+		tags = setOf("0.0.1", "0.0.1.coupon-issuer.01", "latest")
+	}
+	container {
+		creationTime = "USE_CURRENT_TIMESTAMP"
+
+		// JVM 옵션들
+		jvmFlags = listOf("-Dspring.profiles.active=local", "-XX:+UseContainerSupport", "-Dserver.port=8080", "-Dfile.encoding=UTF-8")
+
+		// 컨테이너 입장에서 자기 자신을 외부로 노출할 포트입니다.
+		ports = listOf("8080")
+	}
 }
